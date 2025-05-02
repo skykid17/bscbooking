98ebb522b0b3d7426851fdcba0f388b72ea6b55f
skykid17 <geolai71@gmail.com>
2025-05-03 01:09:58 +0800

    Reset

--- frontend/src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-gray-100 text-gray-600 py-4 text-center border-t">
-            <div className="container mx-auto px-4">
-                <p className="text-sm">© {new Date().getFullYear()} BSC Booking System</p>
-            </div>
-        </footer>
-    );
-}
